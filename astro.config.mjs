3b009e360cb55d772b9c0ae78feb579766ef0677
jeffjuann <jeffjuann@gmail.com>
2024-07-01 16:48:42 +0700

    feat: vercel integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import icon from "astro-icon";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx(), icon()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    mdx(), 
+    icon()
+  ],
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'hover',
-  }
+    defaultStrategy: 'hover'
+  },
+  output: "static",
+  adapter: vercelStatic()
 });
